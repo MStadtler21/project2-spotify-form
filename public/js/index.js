2738b9d7686bb77fda3ab43fc2cc55c9ee610dff
Brad Pritchett <bradpritchett1@msn.com>
2020-08-07 14:10:18 -0600

    build api-routes

--- public/js/index.js
+++ public/js/index.js
@@ -1,31 +1,31 @@
 let token;
 
 (function () {
-  console.log("load");
-  /**
-   * Obtains parameters from the hash of the URL
-   * @return Object
-   */
-  function getHashParams() {
-    var hashParams = {};
-    var e,
-      r = /([^&;=]+)=?([^&;]*)/g,
-      q = window.location.hash.substring(1);
-    while ((e = r.exec(q))) {
-      hashParams[e[1]] = decodeURIComponent(e[2]);
-    }
-    return hashParams;
-  }
-  // get token from url
-  token = getHashParams();
+	console.log("load");
+	/**
+	 * Obtains parameters from the hash of the URL
+	 * @return Object
+	 */
+	function getHashParams() {
+		var hashParams = {};
+		var e,
+			r = /([^&;=]+)=?([^&;]*)/g,
+			q = window.location.hash.substring(1);
+		while ((e = r.exec(q))) {
+			hashParams[e[1]] = decodeURIComponent(e[2]);
+		}
+		return hashParams;
+	}
+	// get token from url
+	token = getHashParams();
 
-  if (token.access_token) {
-    // delete token from url
-    document.location.href = document.location.href.slice(
-      0,
-      document.location.href.indexOf("access_token")
-    );
-  } else {}
+	if (token.access_token) {
+		// delete token from url
+		document.location.href = document.location.href.slice(
+			0,
+			document.location.href.indexOf("access_token")
+		);
+	} else { }
 })();
 console.log(token);
 // (function () {
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,48 @@
+//Get all
+///api/search/:title
+//api/album:id
+//POst /api/comment/:id
 
+var db = require("../models");
+
+module.exports = function (app) {
+
+	app.get("/api/albums", function (req, res) {
+		db.Album.findAll({})
+			.then(function (dbAlbums) {
+				res.json(dbAlbums);
+			});
+	});
+
+	app.get("/api/album:id", function (req, res) {
+		db.Album.findOne({
+			where: {
+				title: req.body.title
+			}
+		}).then(function (dbAlbum) {
+			res.json(dbAlbum);
+		});
+	});
+
+	app.get("/api/search/:title", function (req, res) {
+		db.Album.findOne({
+			where: {
+				id: req.params.title
+			}
+		}).then(function (dbAlbum) {
+			res.json(dbAlbum);
+		});
+	});
+
+	app.post("/api/comment/:id", function (req, res) {
+		console.log("req.body is", req.body);
+		db.Comment.create({
+			text: req.body.text,
+			author: req.body.author
+		})
+			.then(function (dbComment) {
+				console.log(dbComment);
+				res.end();
+			});
+	});
+};
