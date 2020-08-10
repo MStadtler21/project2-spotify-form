fc9e7be6f47c834bc78e02938359ee9a58878b82
MatStat <powdersteamboat@gmail.com>
2020-08-10 14:16:55 -0600

    conflict test

--- public/js/index.js
+++ public/js/index.js
@@ -6,21 +6,6 @@ let token;
    * Obtains parameters from the hash of the URL
    * @return Object
    */
-<<<<<<< HEAD
-	function getHashParams() {
-		var hashParams = {};
-		var e,
-			r = /([^&;=]+)=?([^&;]*)/g,
-			q = window.location.hash.substring(1);
-		while ((e = r.exec(q))) {
-			hashParams[e[1]] = decodeURIComponent(e[2]);
-		}
-		return hashParams;
-	}
-	// get token from url
-	token = getHashParams();
-
-=======
 	$.ajax({
 		url: "/api/albums/all",
 		method: "GET",
@@ -63,73 +48,12 @@ let token;
 	// get token from url
 	token = getHashParams();
 
->>>>>>> 7d8ad6f1b82cf7f544da1993e79a7b4878fd01cd
 	if (token.access_token) {
 		// delete token from url
 		document.location.href = document.location.href.slice(
 			0,
 			document.location.href.indexOf("access_token")
 		);
-<<<<<<< HEAD
-	} else {}
-})();
-console.log(token);
-
-$("#album-search").on("click", function(event) {
-	console.log("test")
-	event.preventDefault();
-
-	var album = $("#search-input").val();
-
-	$.ajax({
-		url: "",
-		method: "GET"
-	}).then(function(response) {
-		$("").text(JSON.stringify(response));
-	});
-
-      
-});
-$(".album-card").on("click", function(event) {
-	console.log("dummy");
-	
-	$.ajax({
-		url: "",
-		method: "GET"
-	}).then(function(response) {
-		$("").text(JSON.stringify(response));
-	});
-
-});
-
-$("#submit-comment").on("click", function(event){
-	console.log("dummy2");
-	
-	$.ajax({
-		url: "",
-		method: "GET"
-	}).then(function(response) {
-		$("").text(JSON.stringify(response));
-	});
-	
-});
-$("#album-add").on("click", function(event) {
-	console.log("albumAdd");
-	var id = $("#id-input").val();
-
-
-	if(token.length>1){
-		$.ajax({
-			url: `/add/${id}/${token}`,
-			method: "GET"
-		}).then(function(response) {
-			$("").text(JSON.stringify(response));
-		});
-	}
-	
-
-}); 
-=======
 
 		$("#login").hide();
 	} else {
@@ -189,4 +113,3 @@ $("#album-add").on("click", function (event) {
 		});
 	}
 });
->>>>>>> 7d8ad6f1b82cf7f544da1993e79a7b4878fd01cd
