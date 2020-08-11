f61b79aac8d06059e413fab3c0a7a4ba17aaf603
cjkook <cjko.audio@gmail.com>
2020-08-10 21:08:18 -0600

    display name prints client-side

--- public/js/index.js
+++ public/js/index.js
@@ -34,46 +34,47 @@ $(function () {
       var title = response[i].title;
       var artist = response[i].artist;
 
-			html += `<div class="album-card bg-green-500 p-8 ml-64 max-w-lg text-center rounded overflow-hidden shadow-lg" data-id="${spotify_id}">`;
-			html += `<img class="w-full" src = "${imgURLMed}" alt = "Album Cover" id = "album-cover-large" />`;
-			html += "<div class=\"px-6 py-4\"></div>";
-			html += "<div class=\"font-bold text-xl mb-2\">";
-			html += `<h3 class="text-black" id="album-title">${title}</h3>`;
-			html += `<h4 class="text-black" id="artist-title">${artist}</h4>`;
-			html += "</div>";
-			html += `<div class="accordion" id="comment-drawer-${spotify_id}" >`;
-			html += `<form class="rounded px-8 pt-6 pb-8 mb-4">`;
-			html += `<div class="mb-4">`;
-			html += "<label class=\"block text-gray-700 text-sm font-bold mb-2\" for=\"comment\">";
-			html += "Comments";
-			html += `</label >`;
-			html += `<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="comment-${spotify_id}" type="text" placeholder="comment">`;
-			html += `</div>`;
-			html += `</form > `;
-			html += `</div>`;
-			html += "</div >";
-		}
-		$("#all-albums").append(html);
-		$(".accordion").hide();
-		$(".album-card").on("click", function (event) {
-			let comment = "";
-			let id = $(this).data("id");
-			console.log(id);
-			$("#comment-drawer-" + id).show();
-			$.ajax({
-				url: `api / albums / ${$(this).data("id")} `,
-				method: "GET",
-				data: { parameters: parameters.access_token, id: $(this).data("id") },
-			}).then(function (response) {
-				console.log(response);
-				// $("").text(JSON.stringify(response));
-			});
-		});
-	}
+      html += `<div class="album-card bg-green-500 p-8 ml-64 max-w-lg text-center rounded overflow-hidden shadow-lg" data-id="${spotify_id}">`;
+      html += `<img class="w-full" src = "${imgURLMed}" alt = "Album Cover" id = "album-cover-large" />`;
+      html += '<div class="px-6 py-4"></div>';
+      html += '<div class="font-bold text-xl mb-2">';
+      html += `<h3 class="text-black" id="album-title">${title}</h3>`;
+      html += `<h4 class="text-black" id="artist-title">${artist}</h4>`;
+      html += "</div>";
+      html += `<div class="accordion" id="comment-drawer-${spotify_id}" >`;
+      html += `<form class="rounded px-8 pt-6 pb-8 mb-4">`;
+      html += `<div class="mb-4">`;
+      html +=
+        '<label class="block text-gray-700 text-sm font-bold mb-2" for="comment">';
+      html += "Comments";
+      html += `</label >`;
+      html += `<input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="comment-${spotify_id}" type="text" placeholder="comment">`;
+      html += `</div>`;
+      html += `</form > `;
+      html += `</div>`;
+      html += "</div >";
+    }
+    $("#all-albums").append(html);
+    $(".accordion").hide();
+    $(".album-card").on("click", function (event) {
+      let comment = "";
+      let id = $(this).data("id");
+      console.log(id);
+      $("#comment-drawer-" + id).show();
+      $.ajax({
+        url: `api / albums / ${$(this).data("id")} `,
+        method: "GET",
+        data: { parameters: parameters.access_token, id: $(this).data("id") },
+      }).then(function (response) {
+        console.log(response);
+        // $("").text(JSON.stringify(response));
+      });
+    });
+  }
 
   // get token from url
   parameters = getHashParams();
-
+  console.log(parameters);
   if (parameters.access_token) {
     // delete token from url
     document.location.href = document.location.href.slice(
