d7bf2aabaa1e2441eb985380ce6b42bb9b031209
macT <simmonst208@gmail.com>
2020-08-10 21:05:53 -0500

    got rid of user table association with comments

--- models/comment.js
+++ models/comment.js
@@ -7,15 +7,22 @@ module.exports = function (sequelize, DataTypes) {
 			validate: {
 				len: [1, 100]
 			}
+		},
+		displayName: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			validate: {
+				len: [1, 50]
+			}
 		}
 	});
 
 	Comment.associate = function (models) {
-		Comment.belongsTo(models.User, {
-			foreignKey: {
-				allowNull: false
-			}
-		});
+	// 	Comment.belongsTo(models.User, {
+	// 		foreignKey: {
+	// 			allowNull: false
+	// 		}
+	// 	});
 		
 		//if we dont like naming, can rename to spotify_id bc will be album id.
 		Comment.belongsTo(models.Album, {
